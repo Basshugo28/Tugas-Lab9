f485022b969209e650a12b5ba5d28f335e54585a
Basten <basten.0407@students.amikom.ac.id>
2020-06-27 15:04:33 +0700

    TugasLab7

---
+++ Tugas Lab 7/Interface/IHandphone.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tugas_Lab_7.Interface
+{
+    public interface IHandphone
+    {
+        public void Buatan();
+    }
+}
